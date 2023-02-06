df237a7476af5c6d11f8fea6435e9a995a824d45
yexin.zheng <yexin.zheng@tcl.com>
2023-02-06 17:06:07 +0800

    feat: core prepare complete

---
+++ utils/get-npm-info/lib/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const axios = require('axios');
+const urlJoin = require('url-join');
+const semver = require('semver');
+
+function getNpmInfo(npmName, registry) {
+  if (!npmName) {
+    return
+  }
+
+  registry = registry || getDefaultRegistry();
+  const npmInfoUrl = urlJoin(registry, npmName);
+
+  return axios.get(npmInfoUrl).then(response => {
+    if (response?.status === 200) {
+      // return Object.keys(response.data?.versions);
+      return response.data?.['dist-tags']?.latest;
+    }
+  }).catch(error => {
+    // Promise.reject(error)
+  })
+  
+}
+
+async function getNpmSemverVersion(baseVersion, npmName, registry) {
+  const versions = await getNpmInfo(npmName, registry);
+  
+  // let semverVersions = [];
+
+  // if (versions && versions.length > 0) {
+  //   semverVersions = versions
+  //     .filter(version => semver.satisfies(version, `^${baseVersion}`))
+  //     .sort((a, b) => {
+  //       // 直接 return semver.gt(b, a) 不生效，sort 比较的是数字
+  //       return semver.gt(b, a) ? 1 : -1;
+  //     })
+  // }
+
+  // return semverVersions[0];
+
+  return versions
+}
+
+function getDefaultRegistry(isOriginal = false) {
+  return isOriginal ? 'https://registry.npmjs.org/' : 'https://registry.npm.taobao.org'
+}
+
+module.exports = {
+  getNpmSemverVersion
+}
